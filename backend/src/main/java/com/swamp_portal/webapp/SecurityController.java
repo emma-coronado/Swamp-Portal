0e7b81f3c48ab82954aba0b66840d4070e47cd5b
Dogan <dogantoro@gmail.com>
2025-10-07 23:42:34 -0500

    CORS oops fix

--- backend/src/main/java/com/swamp_portal/webapp/SecurityController.java
+++ backend/src/main/java/com/swamp_portal/webapp/SecurityController.java
@@ -8,8 +8,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 
