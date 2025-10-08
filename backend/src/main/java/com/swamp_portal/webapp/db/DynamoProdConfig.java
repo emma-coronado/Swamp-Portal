f584353f9e0462771409cde937f3c0e16efc0f31
Dogan <dogantoro@gmail.com>
2025-10-07 22:18:19 -0500

    DB now on AWS

--- backend/src/main/java/com/swamp_portal/webapp/db/DynamoProdConfig.java
+++ backend/src/main/java/com/swamp_portal/webapp/db/DynamoProdConfig.java
@@ -3,13 +3,35 @@ package com.swamp_portal.webapp.db;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 @Configuration
 @Profile("!local")
 public class DynamoProdConfig {
+
     @Bean
     public DynamoDbClient dynamoDbClient() {
-        return DynamoDbClient.create();
+        String region = System.getenv("AWS_REGION");
+        String accessKey = System.getenv("AWS_ACCESS_KEY_ID");
+        String secretKey = System.getenv("AWS_SECRET_ACCESS_KEY");
+        String sessionToken = System.getenv("AWS_SESSION_TOKEN");
+
+        if (region == null) {
+            region = "us-east-1";
+        }
+
+        if (accessKey == null || secretKey == null || sessionToken == null)
+            throw new RuntimeException("Missing one or more AWS Credentials!");
+
+        return DynamoDbClient
+                .builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsSessionCredentials.create(accessKey, secretKey, sessionToken)
+                ))
+                .build();
     }
 }
